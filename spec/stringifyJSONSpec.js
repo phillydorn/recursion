1814a8352c145440a50af1af310bbd481230e4e1
Phil Dornfeld <phillydorn75@gmail.com>
2015-02-14 13:02:36 -0800

    Whoops! Took the function out of stringifyJSONSpec and put it into the right file

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,68 +1,7 @@
 // test cases are described in fixtures.js
 describe('stringifyJSON', function(){
 
-var stringifyJSON = function(value) {
 
-
-  var result = "";
-
-  
-  if (value === null) {
-    result =  "null";
-  }
-
-  else if (value instanceof Array) { 
-
-    function arrayStringify (array, i){  // creates a recursive function that runs stringify through
-                                          // every element in the array
-      if (i === array.length) {
-        return '[' + value + ']';
-      }
-
-      else {
-        array[i] = stringifyJSON (array[i]);
-        return arrayStringify(array, i+1);
-      }
-    }
-    result = arrayStringify(value, 0) ;
-  }
-
-
-  else { 
-    switch (typeof(value)) {
-      case "number" :
-      case "boolean" : 
-        result = value.toString();
-        break;
-      case "string" :
-        result = '"' + value + '"';
-        break;
-      case "object" :
-        var stringPair;
-        result = '{';
-        for (key in value) {
-
-          if (typeof(value[key]) !== 'undefined' &&
-              typeof(value[key]) !== 'function') {
-                stringPair = stringifyJSON(key) + ':' + stringifyJSON(value[key]);
-                if (result !== '{') {
-                  result +=',';
-                }
-                result += stringPair;
-          }
-        }
-        result+= '}';
-    }
-  }
-
-  
-
-
-
-
-
-    return result;
-  }
   it('should match the result of calling JSON.stringify', function(){
 
     stringifiableObjects.forEach(function(test){
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,65 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+
+  var result = "";
+
+  
+  if (obj === null) {
+    result =  "null";
+  }
+
+  else if (obj instanceof Array) { 
+
+    function arrayStringify (array, i){  // creates a recursive function that runs stringify through
+                                          // every element in the array
+      if (i === array.length) {
+        return '[' + obj + ']';
+      }
+
+      else {
+        array[i] = stringifyJSON (array[i]);
+        return arrayStringify(array, i+1);
+      }
+    }
+    result = arrayStringify(obj, 0) ;
+  }
+
+
+  else { 
+    switch (typeof(obj)) {
+      case "number" :
+      case "boolean" : 
+        result = obj.toString();
+        break;
+      case "string" :
+        result = '"' + obj + '"';
+        break;
+      case "object" :
+        var stringPair;
+        result = '{';
+        for (key in obj) {
+
+          if (typeof(obj[key]) !== 'undefined' &&
+              typeof(obj[key]) !== 'function') {
+                stringPair = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+                if (result !== '{') {
+                  result +=',';
+                }
+                result += stringPair;
+          }
+        }
+        result+= '}';
+    }
+  }
+
+  
+
+
+
+
+
+    return result;
+  
 };
